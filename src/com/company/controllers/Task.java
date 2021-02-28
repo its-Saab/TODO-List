05f31ce9af34ee5e45050365cc650e408d019c5d
its-Saab <musaababs@gmail.com>
2021-02-28 23:11:10 +0100

    #6 changed fields to final - deleted unused methods

--- src/com/company/controllers/Task.java
+++ src/com/company/controllers/Task.java
@@ -2,62 +2,31 @@ package com.company.controllers;
 
 
 public class Task {
-    private String title;
-    private String description;
-    private String deadLine;
+    private final String title;
+    private final String description;
+    private final String dueDate;
     private boolean isDone = false;
 
-    public Task(String title, String description, String deadline) {
+    public Task(String title, String description, String dueDate) {
         this.title = title;
         this.description = description;
-        this.deadLine = deadline;
+        this.dueDate = dueDate;
     }
 
-    public Task(String title, String description, String deadline, boolean isDone) {
+    public Task(String title, String description, String dueDate, boolean isDone) {
         this.title = title;
         this.description = description;
-        this.deadLine = deadline;
+        this.dueDate = dueDate;
         this.isDone = isDone;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDeadLine() {
-        return deadLine;
-    }
-
-    public void setDeadLine(String deadLine) {
-        this.deadLine = deadLine;
-    }
-
-    public boolean isDone() {
-        return isDone;
-    }
-
-    public void setDone(boolean done) {
-        isDone = done;
-    }
 
     @Override
     public String toString() {
         return
                 "Title: \n" + title + '\n' +
                         "Description:\n" + description + '\n' +
-                        "Dead Line: \n" + deadLine + '\n' +
+                        "Due date: \n" + dueDate + '\n' +
                         "Status: \n" + (isDone ? "Done" : "Not Done yet")
                 ;
     }
