37618592742fb49e8b6369ca293d3438ae910294
its-Saab <musaababs@gmail.com>
2021-02-28 23:51:29 +0100

    #14 Updt: deleted TaskOperations as it's no longer used

--- src/com/company/controllers/CreateTask.java
+++ src/com/company/controllers/CreateTask.java
@@ -6,7 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class CreateTask extends TaskOperations {
+public class CreateTask {
     Scanner scanner;
     public CreateTask() {
         System.out.print("Enter Project Name > ");
