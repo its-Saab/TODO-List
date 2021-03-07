70d49f2a38a168b73f5d65b90a45c4ff89e7b41d
its-Saab <musaababs@gmail.com>
2021-03-07 02:45:43 +0100

    #38Updt: added Test IsRemovingTest

---
+++ src/IsRemovingTest.java
@@ -0,0 +1,17 @@
+import com.company.controllers.CreateTask;
+import com.company.controllers.Task;
+import com.company.controllers.UpdateFile;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import java.io.File;
+import java.io.IOException;
+
+public class IsRemovingTest {
+    @Test
+    public void isFileRemoved() throws IOException {
+        new CreateTask().writeTask("AJproject.txt", new Task("new task", "buying somethings", "2022-03-15"));
+        new UpdateFile("AJproject.txt").deleteFile();
+        assertFalse(new File("./AJproject.txt").exists());
+    }
+}
