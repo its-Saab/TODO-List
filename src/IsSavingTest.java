29161dd9fc4d044a25681bf032458c405679051f
its-Saab <musaababs@gmail.com>
2021-03-07 02:45:00 +0100

    #37Updt: added Test isSavingTest

---
+++ src/IsSavingTest.java
@@ -0,0 +1,20 @@
+import com.company.controllers.CreateTask;
+import com.company.controllers.Task;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+public class IsSavingTest {
+
+    @Test
+    public void isSavingFile() {
+        String fileName = "MNproject.txt";
+        Task task = new Task("new task", "buying somethings", "2021-02-12");
+        new CreateTask().writeTask(fileName, task);
+        assertTrue(new File("./MNproject.txt").exists());
+    }
+
+
+}
