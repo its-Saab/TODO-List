c47230c6086626b711fd55fd79ffc5fd8a5de777
its-Saab <musaababs@gmail.com>
2021-03-13 14:36:04 +0100

    #46: Clean up unwanted files

--- build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.company"},{"l":"com.company.controllers"},{"l":"com.company.view"}]
--- src/com/company/view/Display.java
+++ src/com/company/view/Display.java
@@ -7,10 +7,11 @@ import java.util.Scanner;
 
 public class Display {
     Scanner scanner;
+
     public void displayTaskList() throws IOException {
         File currentDirectory = new File(".");
-        System.out.println("\033[1;33m"+"How would you like to view Tasks?");
-        System.out.print("1-By name / 2-By due date / 3- return > "+ "\033[0m");
+        System.out.println("\033[1;33m" + "How would you like to view Tasks?");
+        System.out.print("1-By name / 2-By due date / 3- return > " + "\033[0m");
         scanner = new Scanner(System.in);
         int sortMethod = scanner.nextInt();
         if (sortMethod == 1) {
@@ -21,17 +22,18 @@ public class Display {
             new Menu().run();
         }
     }
+
     //TODO add colors to the file lines
     public void displayTask() throws IOException {
         displayTaskList();
         scanner = new Scanner(System.in);
-        System.out.print("\033[1;33m"+"Enter project name to display >"+"\033[0m");
+        System.out.print("\033[1;33m" + "Enter project name to display >" + "\033[0m");
         String projectName = scanner.next() + ".txt";
 
         try (BufferedReader reader = new BufferedReader(new FileReader(projectName))) {
             String line = null;
             while ((line = reader.readLine()) != null) {
-                System.out.printf("\t%s%s\n","\033[1;34m", line);
+                System.out.printf("\t%s%s\n", "\033[1;34m", line);
             }
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + projectName);
