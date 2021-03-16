76636e34a963069fb0fa9392576edfe0a3c70973
its-Saab <musaababs@gmail.com>
2021-03-16 19:44:42 +0100

    #54: Add isTaskUpdated Test with mark as Done test

---
+++ src/Test/com/company/isTaskUpdatedTest.java
@@ -0,0 +1,32 @@
+package Test.com.company;
+
+import com.company.controllers.CreateTask;
+import com.company.controllers.Task;
+import com.company.controllers.UpdateFile;
+import org.junit.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class isTaskUpdatedTest {
+
+    @Test
+    public void isMarkedAsDone() throws IOException {
+        String fileName = "CDproject.txt";
+        Task task = new Task("new test", "this file is to test mark as done", "2021-04-12");
+        new CreateTask().writeTask(fileName, task);
+        new UpdateFile(fileName).setState(7,"Done");
+        var words = Files.lines(Paths.get(fileName))
+                .map(s -> s.split("\\R"))
+                .map(Arrays::asList)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        assertEquals("Done", words.get(7));
+    }
+}
